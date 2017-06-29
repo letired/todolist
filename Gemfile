2f982f16a6a4fd95f56c68c3ac46dccc3e140e33
Spencer Bray <spencerjohnbray@gmail.com>
2017-06-29 13:09:08 +0200

    add simple form

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
 
+gem 'simple_form'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,21 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.create
+      redirect_to tasks_path
+    else
+      render 'tasks/index'
+    end
+  end
+
+  private
+
+  def task_params
+  params.require(:task).permit(:name, :description, :complete)
   end
 end
